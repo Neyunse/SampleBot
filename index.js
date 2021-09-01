a571f4a77aa824c0e5b508c2be116df62e530a1c
KagariSoft-dev <dev@kagarisoft.com>
2021-08-31 23:46:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log("Ready"); // Bot is Ready
+});
+
+client.on("message", async (msg) => {
+  var prefix = "!"; //the bot only respond if this exist in the comand
+  if (msg.content === prefix + "love") {
+    msg.reply("i love you <3");
+  }
+  // e.g: > !love
+  //ouput > i love you <3
+});
+client.login('NzUxOTM0NjExNDQ5MzE1MzI5.X1QTrQ.ZjO33QXQl69Irfun-IE7eWMNDlY');
